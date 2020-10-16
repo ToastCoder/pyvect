74c56ae1abd1834a7d6e01fd14a1b9d73c6a7fbe
Vigneshwar Ravichandar <vicky.pcbasic@gmail.com>
2020-10-16 18:05:58 +0530

    Added various attributes to setup

--- setup.py
+++ setup.py
@@ -1,1 +1,36 @@
+import os
+from codecs import open
+
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as f:
+    long_description = f.read()
+
+setup(
+  name = 'vectoralg',         
+  packages = ['vectoralg'],
+  version = '1.1.0',
+  license='MIT',
+  description = 'Vectoralg (or) Vector Algebra functions is a open source python module created for the purpose of simplifying the vector calculations such as finding the angle between vectors, projection of one vector over the other and much more...!',
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  author = ['Vigneshwar K R','Mouli Shankar M R','Vishal Balaji Sivaraman'],
+  author_email = 'vicky.pcbasic@gmail.com',
+  url = 'https://github.com/ToastCoder/vectoralg',
+  download_url = 'https://github.com/ToastCoder/vectoralg/archive/master.zip',
+  keywords = ['VECTOR', 'VECTOR_ALGEBRA_FUNCTIONS','VECTOR_CALCULATIONS'],
+  install_requires=['numpy'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
 
