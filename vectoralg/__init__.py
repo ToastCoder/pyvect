6b3d3517838dc2d6f2a2d1fde8d80940f06236da
ToastCoder <vicky.pcbasic@gmail.com>
2020-10-17 11:29:02 +0530

    Update __init__.py

--- vectoralg/__init__.py
+++ vectoralg/__init__.py
@@ -1,1 +1,8 @@
+# Module name: vectoralg
+# Short description: Vectoralg (or) Vector Algebra functions is a open source python module created for the purpose of simplifying the vector calculations such as finding the angle between vectors, projection of one vector over the other and much more...!
+# Developers: Vigneshwar Ravichandar(@ToastCoder), Moulishankar M R (@Moulishankar10), Vishal Balaji Sivaraman (@The-SocialLion) 
+# Contact email address: vicky.pcbasic@gmail.com, moulishankarmr@gmail.com, vishalsivaraman5@gmail.com
+# Modules required: numpy
 
+# Command to install vectoralg:
+# >>> pip install vectoralg
