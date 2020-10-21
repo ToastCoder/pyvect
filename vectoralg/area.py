04cff0ea183a86dfb37c550966c5eb605621a29c
Moulishankar M R <moulishankarmr@gmail.com>
2020-10-21 11:41:00 +0530

    Update area.py

--- vectoralg/area.py
+++ vectoralg/area.py
@@ -6,5 +6,9 @@
 # IMPORTING REQUIRED MODULES
 import numpy as np
 
+# triangle_adj() - Returns the area of a triangle where the two adjacent sides of the triangle are given.
+# Syntax: vectoralg.area.triangle_adj(vector_1,vector_2)
+# vector_1 - First adjacent side , vector_2 - Second adjacent side
+# Return type: float
 def triangle_adj(x,y):
     return abs(0.5*np.linalg.norm(np.cross(x,y)))
