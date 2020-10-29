c063ffc1027b2978fac799b11f1dc515d0fedd6d
ToastCoder <vicky.pcbasic@gmail.com>
2020-10-29 21:16:08 +0530

    Added tetrahedron() function

--- vectoralg/cent.py
+++ vectoralg/cent.py
@@ -14,3 +14,10 @@ import numpy as np
 # Return type: float
 def triangle(a,b,c):
     return 0.33*(np.array([a])+np.array([b])+np.array([c]))
+    
+# tetrahedron() - Returns the centroid vector in the tetrahedron based on the the four given positional vectors.
+# Syntax: vectoralg.cent.tetrahedron(p1,p2,p3,p4)
+# p1,p2,p3,p4 - positional vectors of the tetrahedron.
+# Return type: float
+def tetrahedron(a,b,c,d):
+    return 0.25*(np.array([a])+np.array([b])+np.array([c])+np.array([d]))
