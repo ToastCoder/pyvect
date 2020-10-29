10a751e85ac688472265e64aca34d99b03e9e8d6
ToastCoder <vicky.pcbasic@gmail.com>
2020-10-30 00:21:00 +0530

    Minor changes

--- vectoralg/cent.py
+++ vectoralg/cent.py
@@ -11,10 +11,10 @@ import numpy as np
 # triangle() - Returns the centroid vector in the triangle based on the the three given positional vectors.
 # Syntax: vectoralg.cent.triangle(p1,p2,p3)
 # p1,p2,p3 - positional vectors of the triangle.
-# Return type: float
+# Return type: array
 def triangle(a,b,c):
     return 0.33*(np.array([a])+np.array([b])+np.array([c]))
-    
+
 # tetrahedron() - Returns the centroid vector in the tetrahedron based on the the four given positional vectors.
 # Syntax: vectoralg.cent.tetrahedron(p1,p2,p3,p4)
 # p1,p2,p3,p4 - positional vectors of the tetrahedron.
