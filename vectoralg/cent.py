952ddcc755802c8673c93b03c73ce6017292b13c
ToastCoder <vicky.pcbasic@gmail.com>
2020-10-29 21:14:18 +0530

    Added triangle() function

--- vectoralg/cent.py
+++ vectoralg/cent.py
@@ -6,3 +6,11 @@
 # IMPORTING REQUIRED MODULES
 import numpy as np
 
+# FUNCTIONS
+
+# triangle() - Returns the centroid vector in the triangle based on the the three given positional vectors.
+# Syntax: vectoralg.cent.triangle(p1,p2,p3)
+# p1,p2,p3 - positional vectors of the triangle.
+# Return type: float
+def triangle(a,b,c):
+    return 0.33*(np.array([a])+np.array([b])+np.array([c]))
