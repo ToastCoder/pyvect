cae077ce70cf04aab9ef6c6d31c54b7d4c3dc437
ToastCoder <vicky.pcbasic@gmail.com>
2020-11-15 12:00:18 +0530

    Added sk_line()

--- pyvect/dist.py
+++ pyvect/dist.py
@@ -16,3 +16,11 @@ def pl_line(a1,a2,u):
     d = list(np.array(a2)-np.array(a1))
     x = np.linalg.norm(np.cross(u,d))
     return x/np.linalg.norm(u)
+
+# sk_line() - Returns the distance between two skew lines.
+# Syntax: vectoralg.dist.sk_line(a1_vector,a2_vector,u_vector,v_vector)
+# a1_vector, a2_vector - position vectors , u_vector - vector part of arbitrary constant t , v_vector - vector part of arbitrary constant s
+# Return type: float
+def sk_line(a1,a2,u,v):
+    x = np.array([list(np.array(a2)-np.array(a1)),u,v])
+    return abs(np.linalg.det(x))/np.linalg.norm(np.cross(u,v))
