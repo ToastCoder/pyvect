f583c2df40661e071d11026f73446a8e2b9f4cdf
Moulishankar10 <moulishankarmr@gmail.com>
2020-12-10 19:10:19 +0530

    Updated prod.py

--- pyvect/prod.py
+++ pyvect/prod.py
@@ -1,1 +1,16 @@
+# SUBMODULE FOR VECTOR PRODUCT AND SCALAR PRODUCT RELATED FUNCTIONS
 
+# TO USE THE FUNCTIONS IN THIS SUBMODULES:
+# >>> vectoralg.prod
+
+# IMPORTING REQUIRED MODULES
+import numpy as np
+
+# FUNCTIONS
+
+# s3() - Returns the scalar triple product of the given three vectors.
+# Syntax: vectoralg.prod.s3(vector_1,vector_2,vector_3)
+# vector_1 - First vector , vector_2 - Second vector , vector_3 - Third vector
+# Return type: int
+def s3(x,y,z):
+    return (np.dot(np.cross(x,y),z))
