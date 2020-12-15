43d4d837cbdc2897b4788a4b8332332c8e22e432
Moulishankar10 <moulishankarmr@gmail.com>
2020-12-15 18:37:57 +0530

    Modified the code

--- pyvect/prod.py
+++ pyvect/prod.py
@@ -23,7 +23,7 @@ def s4(w,x,y,z):
     return np.dot(np.cross(w,x),np.cross(y,z))
 
 # v3() - Returns the vector triple product of the given three vectors.
-# Syntax: vectoralg.prod.v3(vector_1,vector_2,vector_3)
+# Syntax: pyvect.prod.v3(vector_1,vector_2,vector_3)
 # vector_1 - First vector , vector_2 - Second vector , vector_3 - Third vector
 # Return type: array
 def v3(x,y,z):
