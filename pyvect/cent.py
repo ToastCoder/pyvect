1e01c2cc74a9e20584efca8b41312ed4ac92fe29
Moulishankar10 <moulishankarmr@gmail.com>
2021-01-19 18:54:30 +0530

    Modified the code

--- pyvect/cent.py
+++ pyvect/cent.py
@@ -13,11 +13,11 @@ import numpy as np
 # p1,p2,p3 - positional vectors of the triangle.
 # Return type: array
 def triangle(a,b,c):
-    return 0.33*(np.array([a])+np.array([b])+np.array([c]))
+    return 0.33*(np.array(a)+np.array(b)+np.array(c))
 
 # tetrahedron() - Returns the centroid vector in the tetrahedron based on the the four given positional vectors.
 # Syntax: pyvect.cent.tetrahedron(p1,p2,p3,p4)
 # p1,p2,p3,p4 - positional vectors of the tetrahedron.
 # Return type: array
 def tetrahedron(a,b,c,d):
-    return 0.25*(np.array([a])+np.array([b])+np.array([c])+np.array([d]))
+    return 0.25*(np.array(a)+np.array(b)+np.array(c)+np.array(d))
