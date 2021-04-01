05bf6cb5eba321b1c9f2cd9b69cd947df4e31c4a
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-01 20:18:35 +0530

    Updated section.py

--- pyvect/section.py
+++ pyvect/section.py
@@ -15,3 +15,11 @@ import numpy as np
 # Return type: array
 def internal(a,b,m,n):
     return ((m*np.array([a]))-(n*np.array([b]))/(m-n))
+
+# external() - Returns a vector using section formula using external method
+# Syntax: pyvect.section.external(p1,p2,m,n)
+# p1,p2 - Positional vectors
+# m,n - Paramters of the ratio (m:n)
+# Return type: array
+def external(a,b,m,n):
+    return ((m*np.array([a]))+(n*np.array([b]))/(m+n))
