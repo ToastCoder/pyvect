f7b500d6bb5fa16036e384f58d5c1d22adffb02e
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-01 20:17:33 +0530

    Updated section.py

---
+++ pyvect/section.py
@@ -0,0 +1,17 @@
+# SUBMODULE FOR SECTION VECTOR OPERATIONS
+
+# TO USE THE FUNCTIONS IN THIS SUBMODULE
+# >>> pyvect.section
+
+# IMPORTING REQUIRED MODULES
+import numpy as np
+
+# FUNCTIONS
+
+# internal() - Returns a vector using section formula using internal method
+# Syntax: pyvect.section.internal(p1,p2,m,n)
+# p1,p2 - Positional vectors
+# m,n - Paramters of the ratio (m:n)
+# Return type: array
+def internal(a,b,m,n):
+    return ((m*np.array([a]))-(n*np.array([b]))/(m-n))
