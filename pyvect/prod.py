bb57b4a3c611775ab3dc078933e51c365dcd9d8c
Moulishankar10 <moulishankarmr@gmail.com>
2022-01-07 16:03:19 +0530

    Modify : prod.py -> removed numpy dependicies

--- pyvect/prod.py
+++ pyvect/prod.py
@@ -4,7 +4,8 @@
 # >>> pyvect.prod
 
 # IMPORTING REQUIRED MODULES
-import numpy as np
+from . import __init__
+from .__init__ import *
 
 # FUNCTIONS
 
@@ -13,25 +14,25 @@ import numpy as np
 # vector_1 - First vector , vector_2 - Second vector , vector_3 - Third vector
 # Return type: int
 def s3(x,y,z):
-    return (np.dot(np.cross(x,y),z))
+    return (dot(cross(x,y),z))
 
 # s4() - Returns the scalar product of the given four vectors.
 # Syntax: pyvect.prod.s4(vector_1,vector_2,vector_3,vector_4)
 # vector_1 - First vector , vector_2 - Second vector , vector_3 - Third vector , vector_4 - Fourth vector
 # Return type: int
 def s4(w,x,y,z):
-    return np.dot(np.cross(w,x),np.cross(y,z))
+    return dot(cross(w,x),cross(y,z))
 
 # v3() - Returns the vector triple product of the given three vectors.
 # Syntax: pyvect.prod.v3(vector_1,vector_2,vector_3)
 # vector_1 - First vector , vector_2 - Second vector , vector_3 - Third vector
 # Return type: array
 def v3(x,y,z):
-    return np.cross(np.cross(x,y),z)
+    return cross(cross(x,y),z)
 
 # v4() - Returns the vector product of the given four vectors.
 # Syntax: pyvect.prod.v4(vector_1,vector_2,vector_3,vector_4)
 # vector_1 - First vector , vector_2 - Second vector , vector_3 - Third vector , vector_4 - Fourth vector
 # Return type: array
 def v4(w,x,y,z):
-    return np.cross(np.cross(w,x),np.cross(y,z))
+    return cross(cross(w,x),cross(y,z))
